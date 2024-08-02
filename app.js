c9c515b1be04de9908f4cb1de3a1f75a24cee143
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-02 14:54:30 -0600

    example of completed code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 const args = process.argv.slice(2);
 
-if (args.length !== 3) {
+if (args.length !== 2) {
   console.error(
     "Usage: node app.js <temperature> <weather> \n ie: node 15.2 rainy"
   );
@@ -14,11 +14,32 @@ const [temperature, weather] = args;
 
 /* This example uses nested conditional statements to also give recommendations for different weather states at different temperatures */
 
-// if temperature is below 5 degrees
-// indent __ if the weather is snowy...
-// if temperature is greater than 5 and less than 15 degrees
-// indent __ if the weather is rainy or windy
-// if temperature is between 15 and 25
-// indent __ if the weather is sunny
-// if temperature is greater than 25
-// indent __ if the weather is sunny
+if (temperature < 5) {
+  console.log("- a winter coat");
+  console.log("- a hat and scarf");
+  console.log("- gloves or mitteens");
+  if (weather === "snowy") {
+    console.log("- Snow boots");
+  } else {
+    console.log("- Warm boots or shoes");
+  }
+} else if (temperature >= 5 && temperature <= 15) {
+  console.log("- a light jacket or sweater");
+  if (weather === "rainy" || weather === "windy") {
+    console.log("- a waterproof jacket");
+    console.log("- a hat");
+  }
+} else if (temperature >= 15 && temperature < 25) {
+  console.log("- a t-shirt or blouse");
+  console.log("- Jeans or shorts");
+  if (weather === "sunny") {
+    console.log("- sunglasses and/or a hat");
+  }
+  // implies 25 and above
+} else {
+  console.log("- a tank top or sundress");
+  console.log("- shorts or a skirt");
+  if (weather === "sunny") {
+    console.log("- sunglasses and/or a hat");
+  }
+}
